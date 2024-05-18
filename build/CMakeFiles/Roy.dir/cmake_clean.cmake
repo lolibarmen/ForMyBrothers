7b1adafc4eb2d1d2cb747032f08cb7145dc6bca8
lolibarmen <Barmenovic@yandex.ru>
2024-05-18 14:02:15 +0000

    Add include dir

---
+++ build/CMakeFiles/Roy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Roy.dir/src/main.cpp.o"
+  "CMakeFiles/Roy.dir/src/main.cpp.o.d"
+  "Roy"
+  "Roy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Roy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
